f9683303a0d96d39b423dba83f8d983669c3f0c1
Timothy <31729676+kolesnik-tim@users.noreply.github.com>
2018-07-04 12:31:10 +0300

    fix

--- src/js/import/swiper.js
+++ src/js/import/swiper.js
@@ -14,3 +14,36 @@ var swiper = new Swiper('.swiper-shares', {
     clickable: true,
   },
 });
+
+
+//gallery
+
+var swiper = new Swiper('.swiper-gallery', {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  navigation: {
+    nextEl: '.swiper-gallery-next',
+    prevEl: '.swiper-gallery-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination-gallery',
+    clickable: true,
+  },
+});
+
+
+
+//reviews
+
+var swiper = new Swiper('.swiper-reviews', {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  navigation: {
+    nextEl: '.swiper-reviews-next',
+    prevEl: '.swiper-reviews-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination-reviews',
+    clickable: true,
+  },
+});
