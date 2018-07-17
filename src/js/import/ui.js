33eef57f45820c5dcd485e3a0bc3d178de35c4e8
Timothy <31729676+kolesnik-tim@users.noreply.github.com>
2018-07-17 15:14:53 +0300

    fix

--- src/js/import/ui.js
+++ src/js/import/ui.js
@@ -1,6 +1,15 @@
 import '../lib/selectize.min.js';
 import '../lib/maskedinput.js';
+import modal from 'jquery-modal';
 
+
+//menu open
+$('.menu-open').on('click', function() {
+  $(this).toggleClass('active');
+  $('.header').toggleClass('active');
+  $('.body').fadeToggle();
+  $('.menu').fadeToggle();
+});
 
 //select
 $('select').selectize();
